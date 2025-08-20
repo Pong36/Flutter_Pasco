4a40e8d455b85bdefc6269b91078fcde86a259db
Pong36 <fritzpasco9@gmail.com>
2025-08-20 09:25:24 +0800

    Initial commit

---
+++ lib/Dartpad fundamentals/List.dart
@@ -0,0 +1,4 @@
+void main(){
+List <String> name=["Alex", "Steve"];
+print('$name');
+}
---
+++ lib/Dartpad fundamentals/loop.dart
@@ -0,0 +1,15 @@
+void main(){
+
+// will loop  1 to 5
+for(int i = 0; i<= 5; i++){
+print(i);
+
+
+}
+int count = 1;
+while(count<=5){
+print("count: $count");
+count++;
+
+}
+}
